3c47f1633667bcb2136c48673108f3af2dbdc826
uioporqwerty <nitish.sachar@protonmail.com>
2022-01-15 06:12:53 -0800

    add testing oauth support

--- wakawatch/wakawatch WatchKit Extension/Views/AuthorizationView.swift
+++ wakawatch/wakawatch WatchKit Extension/Views/AuthorizationView.swift
@@ -1,19 +1,38 @@
 import SwiftUI
-import AuthenticationServices
+import BetterSafariView
 
 let callbackURLScheme = "wakawatch"
 let clientId = "59wzFIXtADCSV7Kff7Ck4aha"
-let clientSecret = "sec_6M1lwwSuU8s6kZFBL84dBzXIzFHGaqsNVio9Bh1IbF4jha0pBS6dUvSq4fxjVKu6iya0SXKr5kCEIZ9W"
-
+let clientSecret = "sec_c99U07N5CM91cWjDCu2OQqO8bpqUiwOlGWjnucUVq6oBuc6ED7AipV7uYP8bHuvgBnnVZ8mEhElUByF8"
 
 struct AuthorizationView: View {
     @State var authorized: Bool
+    @State private var startingWebAuthenticationSession = false
+    private let redirectUri = "wakawatch://oauth-callback".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
     
     var body: some View {
         Group {
             if !authorized {
                 VStack {
-                    Button("Connect", action: connectToWakaTime)
+                    Button("Connect", action: { self.startingWebAuthenticationSession = true })
+                        .webAuthenticationSession(isPresented: $startingWebAuthenticationSession) {
+                        WebAuthenticationSession(
+                            url: URL(string: "https://wakatime.com/oauth/authorize?scope=email%2Cread_stats&response_type=code&state=e12df671bf011d5d8c4c41bc45b359f4a7ce49a5&redirect_uri=wakawatch%3A%2F%2Foauth-callback&client_id=59wzFIXtADCSV7Kff7Ck4aha")!,
+                            callbackURLScheme: callbackURLScheme
+                        ) { callbackURL, error in
+                            guard error == nil, let successURL = callbackURL else {
+                               return
+                            }
+                            
+                            let oauthToken = NSURLComponents(string: (successURL.absoluteString))?.queryItems?.filter({$0.name == "code"}).first
+                            print(oauthToken ?? "No OAuth Token")
+                            
+                            let defaults = UserDefaults.standard
+                            authorized = true
+                            defaults.set(true, forKey: DefaultsKeys.authorized)
+                        }
+                        .prefersEphemeralWebBrowserSession(false)
+                    }
                 }
             } else {
                 TabView {
@@ -23,20 +42,6 @@ struct AuthorizationView: View {
                 }
             }
         }
-    }
-    
-    func connectToWakaTime() {
-    //    let authURL = URL(string: "https://wakatime.com/oauth/authorize?client_id=\(clientId)&client_secret=\(clientSecret)&redirect_uri=\(callbackURLScheme)://oauth-callback&response_type=token&scope=email,read_logged_time,read_stats")
-    //
-    //    let authenticationSession = ASWebAuthenticationSession(
-    //      url: authURL!,
-    //      callbackURLScheme: callbackURLScheme) { callbackURL, error in
-    //          print("testing")
-    //    }
-    //
-    //    authenticationSession.start()
-        authorized = true
-        
     }
 }
 
