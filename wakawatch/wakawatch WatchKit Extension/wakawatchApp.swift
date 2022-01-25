6f0b7871b54f0a38826297f090aedf7964094d24
uioporqwerty <nitish.sachar@protonmail.com>
2022-01-25 15:53:06 -0800

    update init

--- wakawatch/wakawatch WatchKit Extension/wakawatchApp.swift
+++ wakawatch/wakawatch WatchKit Extension/wakawatchApp.swift
@@ -2,6 +2,13 @@ import SwiftUI
 
 @main
 struct wakawatchApp: App {
+    init() {
+        #if !DEBUG
+            let apmService = RollbarAPMService()
+            apmService.configure()
+        #endif
+    }
+    
     var body: some Scene {
         WindowGroup {
             ConnectView(connectivityService: ConnectivityService.shared)
--- wakawatch/wakawatch/wakawatchApp.swift
+++ wakawatch/wakawatch/wakawatchApp.swift
@@ -2,6 +2,14 @@ import SwiftUI
 
 @main
 struct wakawatchApp: App {
+    
+    init() {
+        #if !DEBUG
+            let apmService = RollbarAPMService()
+            apmService.configure()
+        #endif
+    }
+    
     var body: some Scene {
         WindowGroup {
             AuthenticationView()
