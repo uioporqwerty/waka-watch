f56c8554d676191670df75d996d1768c9a04bd6e
uioporqwerty <nitish.sachar@protonmail.com>
2022-02-19 16:31:16 -0800

    Add color to refresh button on Summary view close #32

--- wakawatch/wakawatch WatchKit Extension/Views/SummaryView.swift
+++ wakawatch/wakawatch WatchKit Extension/Views/SummaryView.swift
@@ -13,29 +13,34 @@ struct SummaryView: View {
             if self.refreshing || !self.summaryViewModel.loaded {
                 ProgressView()
             } else {
-                HStack {
-                    AsyncButton(action: {
-                        self.refreshing = true
-                        await self.summaryViewModel.getSummary()
-                        self.refreshing = false
-                    }) {
-                        Image(systemName: "arrow.clockwise")
-                    }
-                    .clipShape(Circle())
-                    .frame(width: 32, height: 32)
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
-                .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
-
                 VStack {
                     Text(LocalizedStringKey("SummaryView_Today"))
 
                     Text(summaryViewModel.totalDisplayTime)
                         .multilineTextAlignment(.center)
                         .padding(EdgeInsets(top: 16, leading: 10, bottom: 0, trailing: 10))
-                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+
+                VStack {
+                    AsyncButton(action: {
+                        self.refreshing = true
+                        await self.summaryViewModel.getSummary()
+                        self.refreshing = false
+                    }) {
+                        Image(systemName: "arrow.clockwise")
+                            .padding()
+                            .background(Color.accentColor)
+                            .clipShape(Circle())
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .frame(width: 32, height: 32)
+                }
+                .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
             }
-        }.onAppear {
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onAppear {
             self.summaryViewModel.telemetry.recordViewEvent(elementName: "\(String(describing: SummaryView.self))")
         }
         .task {
