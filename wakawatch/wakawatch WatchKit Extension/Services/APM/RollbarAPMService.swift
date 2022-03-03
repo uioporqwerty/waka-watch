5732bc044aa983b611133457db0ab988b9217497
uioporqwerty <nitish.sachar@protonmail.com>
2022-03-03 15:53:58 -0800

    Improve rollbar support

--- wakawatch/wakawatch WatchKit Extension/Services/APM/RollbarAPMService.swift
+++ wakawatch/wakawatch WatchKit Extension/Services/APM/RollbarAPMService.swift
@@ -13,8 +13,10 @@ class RollbarAPMService: APMService {
         let config = RollbarConfig()
         config.destination.accessToken = Bundle.main.infoDictionary?["ROLLBAR_ACCESS_TOKEN"] as? String ?? ""
         config.destination.environment = Bundle.main.infoDictionary?["ENVIRONMENT"] as? String ?? "Development"
+        config.telemetry.captureLog = true
         config.telemetry.enabled = true
-
+        config.developerOptions.transmit = true
+        
         Rollbar.initWithConfiguration(config)
     }
 
