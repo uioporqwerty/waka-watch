8be1d47abd5f958c4cf6134b9ac6a8d0e341a22b
uioporqwerty <nitish.sachar@protonmail.com>
2022-03-03 05:36:35 -0800

    modify rollbar config

--- wakawatch/wakawatch WatchKit Extension/Services/APM/RollbarAPMService.swift
+++ wakawatch/wakawatch WatchKit Extension/Services/APM/RollbarAPMService.swift
@@ -2,21 +2,20 @@ import RollbarNotifier
 import Foundation
 
 class RollbarAPMService: APMService {
-    private var configuration: RollbarConfig
     private var networkService: NetworkService
     private var logManager: LogManager
 
     init(networkService: NetworkService,
          logManager: LogManager) {
-        self.configuration = RollbarConfig()
         self.networkService = networkService
         self.logManager = logManager
-        self.configuration.destination.accessToken = Bundle.main.infoDictionary?["ROLLBAR_ACCESS_TOKEN"] as? String
-                                                                                                         ?? ""
-        self.configuration.destination.environment = Bundle.main.infoDictionary?["ENVIRONMENT"] as? String
-                                                                                                ?? "Development"
 
-        Rollbar.initWithConfiguration(self.configuration)
+        let config = RollbarConfig()
+        config.destination.accessToken = Bundle.main.infoDictionary?["ROLLBAR_ACCESS_TOKEN"] as? String ?? ""
+        config.destination.environment = Bundle.main.infoDictionary?["ENVIRONMENT"] as? String ?? "Development"
+        config.telemetry.enabled = true
+
+        Rollbar.initWithConfiguration(config)
     }
 
     func setUser() async {
@@ -28,8 +27,6 @@ class RollbarAPMService: APMService {
 
         // swiftlint:disable line_length
         self.logManager.debugMessage("Setting Rollbar user with email \(profile.email ?? ""), id \(profile.id), and display_name \(profile.display_name ?? "")")
-        self.configuration.setPersonId(profile.id, username: profile.display_name ?? "", email: profile.email ?? "")
-        Rollbar.updateConfiguration(self.configuration)
-        Rollbar.reapplyConfiguration()
+        Rollbar.currentConfiguration()?.setPersonId(profile.id, username: profile.display_name ?? "", email: profile.email ?? "")
     }
 }
