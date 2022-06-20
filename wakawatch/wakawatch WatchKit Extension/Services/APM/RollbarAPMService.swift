b60a3d43727c8f5015a57f24ee1d7874f1114571
Nitish Sachar <nitish.sachar@olo.com>
2022-06-19 18:06:03 -0700

    Scrub sensitive fields from rollbar close #55

--- wakawatch/wakawatch WatchKit Extension/Services/APM/RollbarAPMService.swift
+++ wakawatch/wakawatch WatchKit Extension/Services/APM/RollbarAPMService.swift
@@ -4,12 +4,12 @@ import Foundation
 class RollbarAPMService {
     init() {
         let config = RollbarConfig()
+        config.dataScrubber.scrubFields = ["CLIENT_ID", "CLIENT_SECRET", "client_secret", "access_token", "refresh_token"]
         config.destination.accessToken = Bundle.main.infoDictionary?["ROLLBAR_ACCESS_TOKEN"] as? String ?? ""
         config.destination.environment = Bundle.main.infoDictionary?["ENVIRONMENT"] as? String ?? "Development"
         config.telemetry.captureLog = true
         config.telemetry.enabled = true
-        config.developerOptions.transmit = true
-
+            
         Rollbar.initWithConfiguration(config)
     }
 }
