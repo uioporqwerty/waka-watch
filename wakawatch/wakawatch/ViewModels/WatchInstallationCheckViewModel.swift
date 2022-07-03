8402862d8889432fabdc03e0ef08490a8f4eff9c
uioporqwerty <nitish.sachar@protonmail.com>
2022-07-02 20:51:10 -0700

    Improve installation view onboarding experience

--- wakawatch/wakawatch/ViewModels/WatchInstallationCheckViewModel.swift
+++ wakawatch/wakawatch/ViewModels/WatchInstallationCheckViewModel.swift
@@ -1,5 +1,6 @@
 import Foundation
 import WatchConnectivity
+import UIKit
 
 final class WatchInstallationCheckViewModel: ObservableObject {
     @Published var isWatchAppInstalled = WCSession.default.isWatchAppInstalled
@@ -17,6 +18,17 @@ final class WatchInstallationCheckViewModel: ObservableObject {
                                                name: Notification.Name("WatchAppInstallation"),
                                                object: nil
                                               )
+    }
+
+    func openAppleWatchApp() {
+        guard let appleWatchScheme = URL(string: "itms-watchs://bridge:root=GENERAL_LINK") else {
+            self.logManager.errorMessage("Could not open Apple Watch app. Failed to construct URL.")
+            return
+        }
+
+        if UIApplication.shared.canOpenURL(appleWatchScheme) {
+            UIApplication.shared.open(appleWatchScheme)
+        }
     }
 
     @objc func handleWatchAppInstallationStatus(_ notification: NSNotification) {
--- wakawatch/wakawatch/Views/WatchInstallationCheckView.swift
+++ wakawatch/wakawatch/Views/WatchInstallationCheckView.swift
@@ -11,13 +11,32 @@ struct WatchInstallationCheckView: View {
     var body: some View {
         VStack {
             if !self.viewModel.isWatchAppInstalled {
-                Text(LocalizedStringKey("WatchInstallationCheckView_Instructions_Text"))
+                GeometryReader { geometry in
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack {
+                            Text(LocalizedStringKey("WatchInstallationCheckView_Instructions_Text"))
+                                .lineSpacing(4)
+                                .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
 
-                ProgressView()
+                            ProgressView()
 
-                Button(action: { self.viewModel.isWatchAppInstalled = true }) {
-                       Text(LocalizedStringKey("WatchInstallationView_AlreadyInstalled_ButtonLabel"))
-                }.padding(EdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 0))
+                            Button(action: self.viewModel.openAppleWatchApp) {
+                                Text(LocalizedStringKey("WatchInstallationView_OpenAppleWatch_ButtonLabel"))
+                                    .frame(maxWidth: .infinity, minHeight: 34)
+                            }
+
+                            .buttonStyle(.borderedProminent)
+                            .padding(EdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0))
+
+                            Button(action: { self.viewModel.isWatchAppInstalled = true }) {
+                                   Text(LocalizedStringKey("WatchInstallationView_AlreadyInstalled_ButtonLabel"))
+                            }
+                            .buttonStyle(.plain)
+                            .padding(EdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0))
+                        }
+                        .frame(minHeight: geometry.size.height)
+                    }
+                }
             } else {
                 DependencyInjection.shared.container.resolve(AuthenticationView.self)!
             }
