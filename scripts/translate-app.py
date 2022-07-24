8fc656308347e0ce4f31c3b965012e80135b6be7
uioporqwerty <nitish.sachar@protonmail.com>
2022-07-23 19:05:48 -0700

    Add Italian translation

--- scripts/translate-app.py
+++ scripts/translate-app.py
@@ -6,9 +6,6 @@ key = sys.argv[1]
 language_file = sys.argv[2]
 language_code = sys.argv[3]
 
-print(language_file)
-print(language_code)
-
 endpoint = "https://api.cognitive.microsofttranslator.com/"
 location = "westus2"
 path = 'translate'
@@ -32,7 +29,6 @@ with open(language_file, 'r+') as file:
     translated_lines = []
 
     for line in lines:
-        translated_line = []
         translation_key = line[:line.index("=") - 1]
         text_to_translate = line[line.index("=") + 3: len(line) - 3]
         body = [{
