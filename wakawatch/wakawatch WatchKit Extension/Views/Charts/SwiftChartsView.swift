115a848ae4da9e3326e672ce44c6cabe55ac820c
uioporqwerty <nitish.sachar@protonmail.com>
2022-08-12 23:43:07 -0700

    Add coding time Swift Chart

--- wakawatch/wakawatch WatchKit Extension/Views/Charts/SwiftChartsView.swift
+++ wakawatch/wakawatch WatchKit Extension/Views/Charts/SwiftChartsView.swift
@@ -1,40 +1,92 @@
 import SwiftUI
 import Charts
 
+struct DayProjectData {
+    let projectName: String
+    var data: [ProjectData]
+}
+
+struct ProjectData: Identifiable {
+    let id = UUID()
+    let date: Date
+    let total_seconds: Double
+}
+
 @available(watchOSApplicationExtension 9.0, *)
 struct SwiftChartsView: View {
     private var summaryData: [SummaryData]?
     private var size: CGSize?
     private var languages: [SummaryLanguageData] = []
     private var editors: [SummaryEditorData] = []
+    private var projects: [DayProjectData] = []
     
     init(summaryData: [SummaryData]?,
          size: CGSize?
-        ) {
+    ) {
         self.summaryData = summaryData
         self.size = size
-
+        
         for data in summaryData ?? [] {
-            guard let dayLanguages = data.languages else {
-                continue
+            if let dayLanguages = data.languages {
+                self.languages.append(contentsOf: dayLanguages)
             }
-            self.languages.append(contentsOf: dayLanguages)
+    
+            if let dayEditors = data.editors {
+                self.editors.append(contentsOf: dayEditors)
+            }
             
-            guard let dayEditors = data.editors else {
-                continue
+            if let dayProjects = data.projects {
+                for project in dayProjects {
+                    let projectDate = data.range.date ?? ""
+                    guard let projectSeconds = project.total_seconds else {
+                        continue
+                    }
+                    let projectName = project.name ?? ""
+                    
+                    let dayProjectDataIndex = self.projects.firstIndex(where: { $0.projectName == projectName})
+                    if let dayProjectDataIndex = dayProjectDataIndex {
+                        self.projects[dayProjectDataIndex].data.append(ProjectData(date: DateUtility.getDate(date: projectDate)!,
+                                                               total_seconds: projectSeconds))
+                    } else {
+                        self.projects.append(DayProjectData(projectName: projectName,
+                                                            data: [ProjectData(date: DateUtility.getDate(date: projectDate)!,
+                                                                               total_seconds: projectSeconds)]))
+                        
+                    }
+                }
             }
-            self.editors.append(contentsOf: dayEditors)
         }
     }
-
+    
     var body: some View {
         if self.summaryData == nil {
             ProgressView()
         } else {
+            Text(LocalizedStringKey("SwiftCharts_CodingTimeChart_Title"))
+                .font(.system(size: 12))
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 8, trailing: 0))
+            
+            Chart {
+                ForEach(self.projects, id: \.projectName) { projectData in
+                    ForEach(projectData.data) {
+                        BarMark(x: .value("Date", $0.date, unit: .day),
+                                y: .value("Minutes Coded", $0.total_seconds.minute))
+                    }.foregroundStyle(by: .value("Project", projectData.projectName))
+                }
+            }
+            .chartLegend(position: .bottom,
+                         alignment: .bottom,
+                         spacing: 15)
+            .frame(height: self.size?.height)
+            .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
+        
+            Divider()
+                .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+            
             Text(LocalizedStringKey("SwiftCharts_LanguagesChart_Title"))
                 .font(.system(size: 12))
                 .padding(EdgeInsets(top: 0, leading: 0, bottom: 8, trailing: 0))
-
+            
             Chart(self.languages, id: \.name!) {
                 BarMark(x: .value("Language", $0.name!),
                         y: .value("Total Minutes Used", $0.total_seconds?.minute ?? 0))
@@ -58,7 +110,7 @@ struct SwiftChartsView: View {
             Text(LocalizedStringKey("SwiftCharts_EditorsChart_Title"))
                 .font(.system(size: 12))
                 .padding(EdgeInsets(top: 0, leading: 0, bottom: 8, trailing: 0))
-
+            
             Chart(self.editors, id: \.name!) {
                 BarMark(x: .value("Editor", $0.name!),
                         y: .value("Total Minutes Used", $0.total_seconds?.minute ?? 0))
